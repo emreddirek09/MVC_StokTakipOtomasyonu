241d94ea6679a06983b37caf818531f7863ac2da
EMRE DİREK <emredirek1905@gmail.com>
2021-08-14 00:36:56 +0300

    Delete and List

--- MVC_StokTakipOtomasyonu/Controllers/BirimlerController.cs
+++ MVC_StokTakipOtomasyonu/Controllers/BirimlerController.cs
@@ -19,15 +19,42 @@ namespace MVC_StokTakipOtomasyonu.Controllers
         [HttpGet] // Geçiş işlemlerinin yapılacağı alan GET
         public ActionResult Ekle()
         {
-            return View();
+            return View("Kaydet");
         }
 
         [HttpPost] // Ekleme işlerinin yapılacağı alan POST 
-        public ActionResult Ekle(Birimler p)
+        public ActionResult Kaydet(Birimler p)
         {
-            db.Birimler.Add(p);
+            if (p.ID == 0)
+            {
+                db.Birimler.Add(p); //Ekleme yapılacak olan değer ID'si 0 olarak gelir ve kayıt eder
+            }
+            else
+            {
+                db.Entry(p).State = System.Data.Entity.EntityState.Modified; // ID sıfırdan farklı ise güncelleme
+            }
+            
             db.SaveChanges();
             return RedirectToAction("Index"); 
         }
+        public ActionResult GuncelleBilgiGetir(Birimler p)
+        {
+            var model = db.Birimler.Find(p.ID);
+            if (model == null) return HttpNotFound();
+            return View("Kaydet",model);            
+        }
+        public ActionResult SilBilgiGetir(Birimler p)
+        {
+            var model = db.Birimler.Find(p.ID);
+            if (model == null) return HttpNotFound();
+            return View(model);
+        }
+        public ActionResult Sil(Birimler p)
+        {
+            db.Entry(p).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
     }
 }
--- MVC_StokTakipOtomasyonu/Controllers/KategorilerController.cs
+++ MVC_StokTakipOtomasyonu/Controllers/KategorilerController.cs
@@ -10,7 +10,7 @@ namespace MVC_StokTakipOtomasyonu.Controllers
     public class KategorilerController : Controller
     {
         MVC_StokTakipEntities db = new MVC_StokTakipEntities();
-
+      
         public ActionResult Index()
         {
             return View(db.Kategoriler.ToList()); // Listeleme komutu
@@ -23,6 +23,34 @@ namespace MVC_StokTakipOtomasyonu.Controllers
         public ActionResult Ekle2(Kategoriler p) //Yeni Kategori Ekleme Komutu *Ekle.cshtml sayfası form action dan yönlendirme alıyor.
         {
             db.Kategoriler.Add(p); // Ekleme yapacağında ID 0 olarak kabul eder.
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult GuncelleBilgiGetir(Kategoriler p)
+        {
+           var model = db.Kategoriler.Find(p.ID); //ID ye göre veri çekme
+            
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+        public ActionResult Guncelle(Kategoriler p)
+        {
+            db.Entry(p).State = System.Data.Entity.EntityState.Modified; //Güncelleme işlemini yapan komut
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult SilBilgiGetir(Kategoriler p)
+        {
+            var model = db.Kategoriler.Find(p.ID); // Find işlemi ID ye göre arama yapar.
+            if (model == null) return HttpNotFound();        
+            return View(model);
+        }
+        public ActionResult Sil(Kategoriler p)
+        {
+            db.Entry(p).State = System.Data.Entity.EntityState.Deleted; //Silme işlemini yapan komut
             db.SaveChanges();
             return RedirectToAction("Index");
         }
